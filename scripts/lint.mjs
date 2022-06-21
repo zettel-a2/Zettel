22c6dbf9c057d229cd17ef6fc7643b1dfb9192d8
na2na <na2na@na2na.dev>
2022-06-21 17:35:00 +0900

    環境準備...(eslint周りまだ動きません)

---
+++ scripts/lint.mjs
@@ -0,0 +1,27 @@
+import {execa} from 'execa';
+import path from "node:path";
+import url from "node:url";
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+
+(async () => {
+	console.log('linting packages/backend ...');
+	await execa('npm', ['run', 'lint'], {
+		cwd: __dirname + '/../packages/backend',
+		stdout: process.stdout,
+		stderr: process.stderr,
+	});
+
+	console.log('linting packages/client ...');
+	await execa('npm', ['run', 'lint'], {
+		cwd: __dirname + '/../packages/client',
+		stdout: process.stdout,
+		stderr: process.stderr,
+	});
+
+	console.log('linting packages/sw ...');
+	await execa('npm', ['run', 'lint'], {
+		cwd: __dirname + '/../packages/sw',
+		stdout: process.stdout,
+		stderr: process.stderr,
+	});
+})();
