0531c2c5a8751285999379f89cf84a9b2841e27a
na2na-p <na2na@na2na.dev>
2022-07-14 13:34:36 +0000

    Update ルーティングをまともにした

--- packages/backend/src/service/app.service.ts
+++ packages/backend/src/service/app.service.ts
@@ -1,8 +1,45 @@
 import { Injectable } from '@nestjs/common';
+import * as fs from 'fs';
+import { join } from 'path';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  getIndexPage(): string {
+    return fs.readFileSync(
+      join(__dirname, '../../../client/built/index.html'),
+      'utf8',
+    );
+  }
+
+  getInputPage(): string {
+    return fs.readFileSync(
+      join(__dirname, '../../../client/built/input.html'),
+      'utf8',
+    );
+  }
+
+  getListPage(): string {
+    return fs.readFileSync(
+      join(__dirname, '../../../client/built/list.html'),
+      'utf8',
+    );
+  }
+
+  getOutputPage(): string {
+    return fs.readFileSync(
+      join(__dirname, '../../../client/built/output.html'),
+      'utf8',
+    );
+  }
+
+  getAccountPage(): string {
+    return fs.readFileSync(
+      join(__dirname, '../../../client/built/account.html'),
+      'utf8',
+    );
   }
 }
