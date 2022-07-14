f6a50ea653d04b3c18f50c23ae5294d04d944a7e
na2na-p <na2na@na2na.dev>
2022-07-14 14:04:44 +0000

    Update

--- scripts/install.mjs
+++ scripts/install.mjs
@@ -1,20 +1,39 @@
-import {execa} from 'execa';
+import { execa } from 'execa';
 import path from "node:path";
 import url from "node:url";
 const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
 
-(async () => {
-    console.log('installing packages/backend ...');
-    await execa('yarn', ['install'], {
-        cwd: __dirname + '/../packages/backend',
-        stdout: process.stdout,
-        stderr: process.stderr,
-    });
+// 環境変数(production)かどうか確認
+const isProduction = process.env.NODE_ENV === 'production';
 
-    console.log('installing packages/client ...');
-    await execa('yarn', ['install'], {
-        cwd: __dirname + '/../packages/client',
-        stdout: process.stdout,
-        stderr: process.stderr,
-    });
+(async () => {
+    if (process.env.NODE_ENV !== test) {
+        console.log('installing packages/backend ...');
+        await execa('yarn', ['install'], {
+            cwd: __dirname + '/../packages/backend',
+            stdout: process.stdout,
+            stderr: process.stderr,
+        });
+
+        console.log('installing packages/client ...');
+        await execa('yarn', ['install'], {
+            cwd: __dirname + '/../packages/client',
+            stdout: process.stdout,
+            stderr: process.stderr,
+        });
+    } else {
+        console.log('installing packages/backend ...');
+        await execa('yarn', ['install', "--immutable", "--immutable-cache", "--check-cache"], {
+            cwd: __dirname + '/../packages/backend',
+            stdout: process.stdout,
+            stderr: process.stderr,
+        });
+
+        console.log('installing packages/client ...');
+        await execa('yarn', ['install', "--immutable", "--immutable-cache", "--check-cache"], {
+            cwd: __dirname + '/../packages/client',
+            stdout: process.stdout,
+            stderr: process.stderr,
+        });
+    }
 })();
