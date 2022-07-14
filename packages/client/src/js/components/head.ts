005a3d02ab44ce6ed0448226634886976578676f
na2na-p <na2na@na2na.dev>
2022-07-14 07:44:32 +0000

    Fix faviconが出るように

---
+++ packages/client/src/js/components/head.ts
@@ -0,0 +1,12 @@
+// import logoImg from '@img/logo.png'
+document.createElement('heade').innerHTML = `
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Kiwi+Maru&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Kaushan+Script" rel="stylesheet">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Zettel?</title>
+`
+export {}
