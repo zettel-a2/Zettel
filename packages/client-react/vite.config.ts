5b90703c9a91a335dae3f21b556e53ccd462c6f2
na2na-p <na2na@na2na.dev>
2022-07-15 05:54:01 +0000

    Update

--- packages/client-react/vite.config.ts
+++ packages/client-react/vite.config.ts
@@ -1,14 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import {defineConfig} from 'vite';
+import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
+const htmls = [
+  'index.html',
+  'list.html',
+];
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-		react(),
-		tsconfigPaths()
-	],
-	build: {
-    outDir: '../built', // 出力場所の指定
-  }
-})
+    react(),
+    tsconfigPaths(),
+  ],
+  build: {
+    outDir: './built', // 出力場所の指定
+    rollupOptions: {
+      input: htmls, // 入力ファイルの指定
+    },
+    emptyOutDir: true,
+  },
+});
