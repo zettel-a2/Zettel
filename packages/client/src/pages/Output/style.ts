addfbc5685d50a748b6c0ad7682b2d79be175c4a
na2na-p <na2na@na2na.dev>
2022-09-02 16:59:52 +0900

    Add output

---
+++ packages/client/src/pages/Output/style.ts
@@ -0,0 +1,25 @@
+/* eslint-disable quote-props */
+import {css} from '@emotion/react';
+
+export default {
+  mainTitle: css({
+    color: '#6A5050',
+    margin: '60px auto',
+    textAlign: 'center',
+    width: '60%',
+  }),
+  output: css({
+    width: '100%',
+    height: '60px',
+    padding: '0 1em',
+    margin: '2em auto',
+    color: '#6a5050',
+    borderRadius: '20px',
+    border: 'solid 2px #6a5050', /* 点線*/
+    background: '#fff',
+    /* box-shadow: 0 2px 5px rgba(81, 51, 30, 0.689); */
+    textAlign: 'center',
+    boxShadow: '0 10px 15px 0 rgba(112, 77, 56, 0.5)',
+
+  }),
+};
